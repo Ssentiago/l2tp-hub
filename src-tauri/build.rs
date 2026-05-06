7e5cb69de4db89d059ec2aa3820d1b9f19a10e4c
Sentiago <arseny317@gmail.com>
2026-05-06 11:48:06 +0300

    W

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,6 +4,18 @@ fn main() {
         let mut windows = tauri_build::WindowsAttributes::new();
         windows = windows.app_manifest(r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+        type="win32"
+        name="Microsoft.Windows.Common-Controls"
+        version="6.0.0.0"
+        processorArchitecture="*"
+        publicKeyToken="6595b64144ccf1df"
+        language="*"
+      />
+    </dependentAssembly>
+  </dependency>
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
       <requestedPrivileges>
@@ -12,7 +24,7 @@ fn main() {
     </security>
   </trustInfo>
 </assembly>
-"#);
+"#);;
         tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
             .expect("failed to run build script");
         return;
